b94f9b29875636a7149a8a867f190c5207db7f62
Sangddong <hehe74029@gmail.com>
2023-11-26 16:24:12 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/shop_practice/ShopPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shop_practice/ShopPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
